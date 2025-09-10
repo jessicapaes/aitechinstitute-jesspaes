22f48a216664ef5614398772dbdcf85c605f68dc
jessicapaes <jessicalopes.paes@gmail.com>
2025-09-10 19:22:30 +1000

    week 2 and 3 exercises

---
+++ week_2/notebooks/application.py
@@ -0,0 +1,12 @@
+
+import streamlit as st
+
+st.title(" My First Dashboard!")
+st.write("If you can see this, you're already a Streamlit developer!")
+st.balloons()  # Fun surprise!
+
+# To run this:
+# 1. Save as app_01_hello.py
+# 2. Open terminal
+# 3. Type: streamlit run app_01_hello.py
+# 4. Your browser will open automatically!
